83d27624dc050e61c21a4f8d11b653c63c2f760f
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-07-28 18:39:21 +0300

    Update init.c

--- init.c
+++ init.c
@@ -1,7 +1,6 @@
-// xylen-init (0.3b)
+
+// xylen-init (0.3c)
 // written by VeryEpicKebap
-
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,42 +35,26 @@ void setup_mdev() {
     }
 }
 
-void spawn_shell(const char *path) {
-    struct stat st;
-    if (stat(path, &st) < 0) {
-        fprintf(stderr, "no %s (%s)\n", path, strerror(errno));
-        return;
-    }
-    if (!(st.st_mode & S_IXUSR)) {
-        fprintf(stderr, "not executable: %s\n", path);
-        return;
-    }
-
+void launch_tty(const char *tty, const char *shell) {
     pid_t pid = fork();
     if (pid == 0) {
-        int fd = -1;
-        const char *tty_devices[] = {"/dev/tty1", "/dev/ttyS0", "/dev/console", NULL};
-
-        for (int i = 0; tty_devices[i]; i++) {
-            fd = open(tty_devices[i], O_RDWR);
-            if (fd >= 0) break;
+        int fd = open(tty, O_RDWR);
+        if (fd < 0) {
+            fprintf(stderr, "open %s: %s\n", tty, strerror(errno));
+            _exit(1);
         }
 
-        if (fd >= 0) {
-            dup2(fd, STDIN_FILENO);
-            dup2(fd, STDOUT_FILENO);
-            dup2(fd, STDERR_FILENO);
-            setsid();
-            ioctl(fd, TIOCSCTTY, 0);
-            close(fd);
-        }
+        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        dup2(fd, STDERR_FILENO);
+        close(fd);
 
-        execl(path, path, NULL);
-        fprintf(stderr, "exec %s: %s\n", path, strerror(errno));
-        exit(1);
-    } else if (pid > 0) {
-        int status;
-        waitpid(pid, &status, 0);
+        setsid();
+        ioctl(STDIN_FILENO, TIOCSCTTY, 0);
+
+        execl(shell, shell, NULL);
+        fprintf(stderr, "exec %s: %s\n", shell, strerror(errno));
+        _exit(1);
     }
 }
 
@@ -81,7 +64,7 @@ void sig_handler(int sig) {
 }
 
 int main(void) {
-    printf("\x1b[36m [ xylen-init (v0.3b) ] \x1b[0m \n\n\x1b[32mWelcome!\x1b[0m\n");
+    printf("\x1b[36m [ xylen-init (v0.3c) ] \x1b[0m \n\n\x1b[32mWelcome!\x1b[0m\n");
 
     if (mount(NULL, "/", NULL, MS_REMOUNT, NULL) < 0) {
         fprintf(stderr, "remount / rw: %s\n", strerror(errno));
@@ -95,10 +78,17 @@ int main(void) {
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
 
+    const char *shell = "/bin/xsh";
+    struct stat st;
+    if (stat(shell, &st) != 0)
+        shell = "/bin/sh";
+
+    launch_tty("/dev/tty1", shell);
+    launch_tty("/dev/tty2", shell);
+
     while (1) {
-        spawn_shell("/bin/xsh");
-        spawn_shell("/bin/sh");
-        sleep(1);
+        int status;
+        wait(&status);
     }
 
     return 0;
